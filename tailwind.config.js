3e7d3e22a779041d1e0b0fe343afa5e9bd48f041
TecHeReTiC3141 <talex73@mail.ru>
2023-08-29 14:50:29 +0300

    Revision page is in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        chestnut: "#973F29",
+      }
+    },
   },
   plugins: [],
 }
