0b24712e50759e0e41af0ccb05392ccb28b51081
TecHeReTiC3141 <talex73@mail.ru>
2023-08-31 21:40:52 +0300

    Services section has bee created

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { services } from '../constants'
+import ServiceCard from '../components/ServiceCard'
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
+      <section className="max-container flex  justify-center flex-wrap gap-8">
+        {
+          services.map((serv, ind) => {
+            return <ServiceCard {...serv} key={ind}/>
+          })
+        }
+      </section>
+    )
 }
 
 export default Services
