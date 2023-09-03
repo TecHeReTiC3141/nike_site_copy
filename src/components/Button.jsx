8eb544d906654f74c5a0f620e27ab3a48428c6af
TecHeReTiC3141 <talex73@mail.ru>
2023-09-03 15:24:04 +0300

    Buttons are now more interactive

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Button = ({ text, icon, backgroundColor="bg-coral-red", borderColor="border-coral-red",
   textColor="text-white" }) => {
   return (
-    <button className={`px-6 py-3 rounded-full  flex gap-6 items-center font-montserrat leading-none text-lg border
+    <button className={`px-6 py-3 rounded-full  flex gap-6 items-center font-montserrat leading-none text-lg border relative 
+    hover:shadow-md hover:brightness-95 active:top-1 transtiion-all ease-linear duration-300
     ${backgroundColor} ${borderColor} ${textColor}`}>
         {text}
         {icon && <img src={icon} alt="icon" className="rounded-full" />}
